d16dbf8f8fc058e7b94d38935536cb4e38c8f1d5
Sofia Wicker <sofiazw@gmail.com>
2017-06-10 16:57:15 -0400

    update

--- assets/javascripting/game.js
+++ assets/javascripting/game.js
@@ -1,28 +1,53 @@
 // start with 7 characters
 
+//edit char ter text
+
 $(document).ready(function() {
 
-	var clickedOn = '';
+// Global variables
 
+	var clickedOn = ''; 
+	var clickedOnFight = '';
+
+// click on character to choose a character
+// When you click on a character, the other characters disapear and the enemies appear
 
 	$('.characters').on('click', function() {
 		clickedOn = $(this).attr("src");
 		mainCharacter();
+		return enemies();
 
 		});
 
-// click on character to choose a character
+//clicking on an enemy moves them to defender area
 
-// When you click on a character, the other character disapear and the enemies appear
+	$('.badGuysContainer').on('click', '.badCharacters', function() {
+		clickedOnFight = $(this).attr("src");
+		enemyMove();
+		console.log("Am I working?")
+	});
+
+
+
+// main character
 
 function mainCharacter() {
-	$(".imgCharacterContainer").html("<img class='characters' src='" + clickedOn + "' id='character1'/>");
+	$(".imgCharacterContainer").html("<img class='characters' src='" + clickedOn + "' id='player'/>");
 }
 
+//enemies appear
+
 function enemies() {
-	$(".badGuysCharacter").html
+	$(".badGuysContainer").html("<img class='badCharacters' src= 'assets/images/enemies/borg.jpg' id='badCharacter1'/> <img class='badCharacters' src= 'assets/images/enemies/borgwoman.jpg' id='badCharacter2'/> <img class='badCharacters' src= 'assets/images/enemies/Ferengi.jpg' id='badCharacter3'/> <img class='badCharacters' src= 'assets/images/enemies/gul.jpg' id='badCharacter4'/> <img class='badCharacters' src= 'assets/images/enemies/Qq.jpg' id='badCharacter5'/> <img class='badCharacters' src= 'assets/images/enemies/the dominion.jpg' id='badCharacter6'/> <img class='badCharacters' src= 'assets/images/enemies/romulans.jpg' id='badCharacter7'/>");
 }
-//clicking on an enemy moves them to defender area
+
+
+//moved to defender area
+
+function enemyMove() {
+	$(".badGuysImg").html("<img class='badCharacters' src='" + clickedOnFight + "' id='fighting'/>")
+	$(".badGuysContainer").remove("<img class='badCharacters' src='" + clickedOnFight + "' id='fighting'/>");
+}
 
 //once in the defender area, you are able to fight the enemy character. 
 
@@ -30,7 +55,6 @@ function enemies() {
 
 //Goal is to fight all till you win.  
 
-// Global variables
 
 
 });
