72aa3c5e9d85d1f8ba6382565cec936e66f409e5
Sofia Wicker <sofiazw@gmail.com>
2017-06-15 16:31:50 -0400

    update

--- assets/javascripting/game.js
+++ assets/javascripting/game.js
@@ -4,11 +4,54 @@
 
 $(document).ready(function() {
 
-// Global variables
+// Global variables for moving
 
-	var clickedOn = ''; 
-	var clickedOnFight = '';
+	var clickedOn; 
+	var clickedOnFight;
 
+//global variables for fighting functions
+
+	var mainCharHp = 150;
+	var mainCharStrength = 15;
+
+	var badCharHp;
+	var badCharStrength;
+
+	var wins;
+
+//Barcharacter HPs 
+var badCharacters = [
+		{
+		charname: "borg",
+	 	HP: 150,
+	 	strength: 15,
+		src: "<img class='badCharacters' src= 'assets/images/enemies/borg.jpg' id='borg'>"},
+		{
+		charname: "ferengi",
+		HP: 125,
+		strength: 5,
+		src: "<img class='badCharacters' src= 'assets/images/enemies/Ferengi.jpg' id='ferengi'/>"},
+		{
+		charname: "gul",
+		HP: 175,
+		strength: 25,
+		src: "<img class='badCharacters' src= 'assets/images/enemies/gul.jpg' id='gul'/>"},
+		{
+		charname: "Qq",
+		HP: 250,
+		strength: 50,
+		src: "<img class='badCharacters' src= 'assets/images/enemies/Qq.jpg' id='Qq'/> "}
+		,{
+		charname: "dominion",
+		HP: 200,
+		strength: 40,
+		src: "<img class='badCharacters' src= 'assets/images/enemies/the dominion.jpg' id='dominion'/> "},
+		{
+		charname: "romulans",
+		HP: 150,
+		strength: 30,
+		src: "<img class='badCharacters' src= 'assets/images/enemies/romulans.jpg' id='romulans'/>"}
+	]
 // click on character to choose a character
 // When you click on a character, the other characters disapear and the enemies appear
 
@@ -19,12 +62,11 @@ $(document).ready(function() {
 
 		});
 
-//clicking on an enemy moves them to defender area
+//clicking on an enemy moves them to defender area '.badCharacters'
 
 	$('.badGuysContainer').on('click', '.badCharacters', function() {
 		clickedOnFight = $(this).attr("src");
 		enemyMove();
-		console.log("Am I working?")
 	});
 
 
@@ -38,7 +80,7 @@ function mainCharacter() {
 //enemies appear
 
 function enemies() {
-	$(".badGuysContainer").html("<img class='badCharacters' src= 'assets/images/enemies/borg.jpg' id='badCharacter1'/> <img class='badCharacters' src= 'assets/images/enemies/borgwoman.jpg' id='badCharacter2'/> <img class='badCharacters' src= 'assets/images/enemies/Ferengi.jpg' id='badCharacter3'/> <img class='badCharacters' src= 'assets/images/enemies/gul.jpg' id='badCharacter4'/> <img class='badCharacters' src= 'assets/images/enemies/Qq.jpg' id='badCharacter5'/> <img class='badCharacters' src= 'assets/images/enemies/the dominion.jpg' id='badCharacter6'/> <img class='badCharacters' src= 'assets/images/enemies/romulans.jpg' id='badCharacter7'/>");
+	$(".badGuysContainer").html("<img class='badCharacters' src= 'assets/images/enemies/borg.jpg' id='borg'/> <img class='badCharacters' src= 'assets/images/enemies/Ferengi.jpg' id='ferengi'/> <img class='badCharacters' src= 'assets/images/enemies/gul.jpg' id='gul'/> <img class='badCharacters' src= 'assets/images/enemies/Qq.jpg' id='Qq'/> <img class='badCharacters' src= 'assets/images/enemies/the dominion.jpg' id='dominion'/> <img class='badCharacters' src= 'assets/images/enemies/romulans.jpg' id='romulans'/>");
 }
 
 
@@ -46,15 +88,65 @@ function enemies() {
 
 function enemyMove() {
 	$(".badGuysImg").html("<img class='badCharacters' src='" + clickedOnFight + "' id='fighting'/>")
-	$(".badGuysContainer").remove("<img class='badCharacters' src='" + clickedOnFight + "' id='fighting'/>");
+	updateCharHp();
 }
 
-//once in the defender area, you are able to fight the enemy character. 
+//click on character in defender area to fight them
 
-//Every time you fight(click), your attack power goes up. 
+function enemyClickFight() {
+	$(".clickAttack").on("click", function()
+	fight();
+)}
 
-//Goal is to fight all till you win.  
+//update bad char with correct hp
 
+function updateCharHp() {
+	for(i = 0; i < badCharacters.length; i++) {
+		if (clickedOnFight == badCharacters[i].src) {
+		badCharHp = badCharacters[i].HP;
+		badCharStrength = badCharacters[i].strength;}
+		console.log(badCharHp)
+	}
+}
+//when you click on the char to fight them, ++ the main fight power and subtract strength from Hp.
 
+	function fight() {
+		mainCharHp = mainCharHp - badCharStrength;
+		mainCharStrength = mainCharStrength + 5;
+		badCharHp = badCharHp - mainCharStrength;
+	}
+
+// if hp = zero for main char, you die
+
+function die(){
+ if (mainCharHp == 0)
+ 	alert("Oh no! You died!");
+
+ 	// Global variables for moving
+
+	var clickedOn; 
+	var clickedOnFight;
+
+	//global variables for fighting functions
+
+	var mainCharHp = 150;
+	var mainCharStrength = 15;
+
+	var badCharHp;
+	var badCharStrength;
+	//also reset HP
+}
+
+//if you defeat 3 bad guys, you win!
+
+function win() {
+	for( i=0; i <= wins; i++) {
+		if (badCharHp == 0);
+		wins++;
+	} else if (wins == 3){
+		alert("You Win!");
+	}
+
+} 
 
 });
