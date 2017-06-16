2b44b67ef92a472827b53a128d7721d9d46bce1d
Sofia Wicker <sofiazw@gmail.com>
2017-06-16 11:08:50 -0400

    local commit

--- assets/javascripting/game.js
+++ assets/javascripting/game.js
@@ -4,70 +4,32 @@
 
 $(document).ready(function() {
 
-// Global variables for moving
+// Global variables
 
 	var clickedOn; 
 	var clickedOnFight;
+	var hp;
+	var fightPower;
+	
 
-//global variables for fighting functions
 
-	var mainCharHp = 150;
-	var mainCharStrength = 15;
 
-	var badCharHp;
-	var badCharStrength;
-
-	var wins;
-
-//Barcharacter HPs 
-var badCharacters = [
-		{
-		charname: "borg",
-	 	HP: 150,
-	 	strength: 15,
-		src: "assets/images/enemies/borg.jpg"},
-		{
-		charname: "ferengi",
-		HP: 125,
-		strength: 5,
-		src: "assets/images/enemies/Ferengi.jpg"},
-		{
-		charname: "gul",
-		HP: 175,
-		strength: 25,
-		src: "assets/images/enemies/gul.jpg"},
-		{
-		charname: "Qq",
-		HP: 250,
-		strength: 50,
-		src: "assets/images/enemies/Qq.jpg"}
-		,{
-		charname: "dominion",
-		HP: 200,
-		strength: 40,
-		src: "assets/images/enemies/the dominion.jpg"},
-		{
-		charname: "romulans",
-		HP: 150,
-		strength: 30,
-		src: "assets/images/enemies/romulans.jpg"}
-	]
 // click on character to choose a character
 // When you click on a character, the other characters disapear and the enemies appear
 
 	$('.characters').on('click', function() {
 		clickedOn = $(this).attr("src");
 		mainCharacter();
-		console.log(clickedOn);
 		return enemies();
+
 		});
 
-//clicking on an enemy moves them to defender area '.badCharacters'
+//clicking on an enemy moves them to defender area
 
 	$('.badGuysContainer').on('click', '.badCharacters', function() {
 		clickedOnFight = $(this).attr("src");
 		enemyMove();
-		console.log(clickedOnFight);
+		console.log("Am I working?")
 	});
 
 
@@ -81,7 +43,7 @@ function mainCharacter() {
 //enemies appear
 
 function enemies() {
-	$(".badGuysContainer").html("<img class='badCharacters' src= 'assets/images/enemies/borg.jpg' id='borg'/> <img class='badCharacters' src= 'assets/images/enemies/Ferengi.jpg' id='ferengi'/> <img class='badCharacters' src= 'assets/images/enemies/gul.jpg' id='gul'/> <img class='badCharacters' src= 'assets/images/enemies/Qq.jpg' id='Qq'/> <img class='badCharacters' src= 'assets/images/enemies/the dominion.jpg' id='dominion'/> <img class='badCharacters' src= 'assets/images/enemies/romulans.jpg' id='romulans'/>");
+	$(".badGuysContainer").html("<img class='badCharacters' src= 'assets/images/enemies/borg.jpg' id='badCharacter1'/> <img class='badCharacters' src= 'assets/images/enemies/borgwoman.jpg' id='badCharacter2'/> <img class='badCharacters' src= 'assets/images/enemies/Ferengi.jpg' id='badCharacter3'/> <img class='badCharacters' src= 'assets/images/enemies/gul.jpg' id='badCharacter4'/> <img class='badCharacters' src= 'assets/images/enemies/Qq.jpg' id='badCharacter5'/> <img class='badCharacters' src= 'assets/images/enemies/the dominion.jpg' id='badCharacter6'/> <img class='badCharacters' src= 'assets/images/enemies/romulans.jpg' id='badCharacter7'/>");
 }
 
 
@@ -89,69 +51,19 @@ function enemies() {
 
 function enemyMove() {
 	$(".badGuysImg").html("<img class='badCharacters' src='" + clickedOnFight + "' id='fighting'/>")
-	updateCharHp();
 }
 
-//click on character in defender area to fight them
+//once in the defender area, you are able to fight the enemy character.
 
-function enemyClickFight() {
-	$(".clickAttack").click( function() {
-	fight();
-	die();
-	win();
-	}
-)};
+//Each enemy character gets a random number 100 through 195
 
-//update bad char with correct hp
+//Each enemy has a specific hp and fight power - hp does not go up. 
 
-function updateCharHp() {
-	for(i = 0; i < badCharacters.length; i++) {
-		if (clickedOnFight == badCharacters[i].src) {
-		badCharHp = badCharacters[i].HP;
-		badCharStrength = badCharacters[i].strength;}
-		console.log(badCharHp)
-	}
-}
-//when you click on the char to fight them, ++ the main fight power and subtract strength from Hp.
+//each good guy has a specific hp and fight power - fight power goes up by 15hp each time.
 
-	function fight() {
-		mainCharHp = mainCharHp - badCharStrength;
-		mainCharStrength = mainCharStrength + 5;
-		badCharHp = badCharHp - mainCharStrength;
-	}
 
-// if hp = zero for main char, you die
+//Goal is to fight all till you win.  
 
-function die(){
- if (mainCharHp == 0)
- 	alert("Oh no! You died!");
 
- 	// Global variables for moving
-
-	var clickedOn; 
-	var clickedOnFight;
-
-	//global variables for fighting functions
-
-	var mainCharHp = 150;
-	var mainCharStrength = 15;
-
-	var badCharHp;
-	var badCharStrength;
-	//also reset HP
-}
-
-//if you defeat 3 bad guys, you win!
-
-function win() {
-	for( i=0; i <= wins; i++) {
-		if (badCharHp == 0) {
-		wins++;
-		} else if (wins == 3) {
-		alert("You Win!");
-		}
-
-	} 
-}
 
 });
